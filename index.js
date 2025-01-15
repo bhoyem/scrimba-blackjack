2f8fd0c4e6be9e1619c773baa700cdf99c904bd6
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-15 15:40:12 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.floor((Math.random() * 10) + 2);
+let secondCard = Math.floor((Math.random() * 10) + 2);
+
+let sum = firstCard + secondCard;
+
+console.log(`first card: ${firstCard}`);
+console.log(`second card: ${secondCard}`);
+console.log(`Sum: ${sum}`);
