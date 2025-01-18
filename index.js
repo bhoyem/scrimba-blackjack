24dada808634eba7dccb2c022af5ed8125c59713
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-18 07:24:51 -0600

    Add start game button
    Created button to start game

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
 let firstCard = Math.floor((Math.random() * 10) + 2);
 let secondCard = Math.floor((Math.random() * 10) + 2);
 
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
 console.log(`first card: ${firstCard}`);
 console.log(`second card: ${secondCard}`);
 console.log(`Sum: ${sum}`);
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false
+    }
+    console.log(message);
+    return;
+}
