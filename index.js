953767a40c5348fd18bee4f927e097588638e8a4
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-19 08:09:39 -0600

    Add background image and sum function to display
    Added the felt type background image and added code to display the sum of the current cards in the display

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 let firstCard = Math.floor((Math.random() * 10) + 2);
 let secondCard = Math.floor((Math.random() * 10) + 2);
-
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
 console.log(`first card: ${firstCard}`);
 console.log(`second card: ${secondCard}`);
 console.log(`Sum: ${sum}`);
 
 function startGame() {
+    sumEl.textContent = "Sum: "+sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "You've got Blackjack!";
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
         isAlive = false
     }
-    console.log(message);
+    messageEl.textContent = message;
     return;
 }
